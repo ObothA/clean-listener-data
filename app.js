b6cd34bbf123cf4715e6416555ab170c0cb4a5fb
Obotha <khyper234@gmail.com>
2019-02-10 04:08:36 +0300

    Test arrow function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import 'babel-polyfill'; // required to make promises work
 import mysql from 'mysql';
 
+import queryTable from './utils/queryTable';
+
 const CONNECTION = mysql.createConnection({
   host: 'localhost',
   user: 'jmuhumuza',
@@ -16,36 +18,6 @@ CONNECTION.connect((connectionError) => {
   console.log('CONNECTION ESTABLISHED ************************* ========================== *********************** ');
 });
 
-
-const queryTable = async (table) => {
-  const query = `SELECT RTC_T,id FROM ${table} WHERE CHAR_LENGTH(RTC_T) > 19`;
-  console.log(`starting to clean ${table}`);
-
-  await CONNECTION.query(query, (queryError, result, fields) => {
-    if (queryError) {
-      throw queryError;
-    }
-    result.map(async (rtcField) => {
-      // console.log(`${rtcField.id} === ${rtcField.RTC_T.slice(-19)}`);
-      console.log(`cleaning ${table} data ....`);
-      const { id } = rtcField;
-      const RTC_T = rtcField.RTC_T.slice(-19);
-
-      const updateQuery = `UPDATE ${table} SET RTC_T = '${RTC_T}' WHERE id = ${id}`;
-
-      await CONNECTION.query(updateQuery, (updateError, result, fields) => { 
-        if (updateError) {
-          throw updateError;
-        }
-      });
-
-      return undefined;
-    });
-
-    console.log(`############ done cleaning ${table}`);
-    console.log();
-    console.log();
-  });
-};
-
 queryTable('GroundNode');
+
+export default { CONNECTION };
---
+++ utils/queryTable.js
@@ -0,0 +1,34 @@
+
+
+const queryTable = async (table) => {
+  const query = `SELECT RTC_T,id FROM ${table} WHERE CHAR_LENGTH(RTC_T) > 19`;
+  console.log(`starting to clean ${table}`);
+
+  await CONNECTION.query(query, (queryError, result, fields) => {
+    if (queryError) {
+      throw queryError;
+    }
+    result.map(async (rtcField) => {
+      // console.log(`${rtcField.id} === ${rtcField.RTC_T.slice(-19)}`);
+      console.log(`cleaning ${table} data ....`);
+      const { id } = rtcField;
+      const RTC_T = rtcField.RTC_T.slice(-19);
+  
+      const updateQuery = `UPDATE ${table} SET RTC_T = '${RTC_T}' WHERE id = ${id}`;
+  
+      await CONNECTION.query(updateQuery, (updateError, result, fields) => { 
+        if (updateError) {
+          throw updateError;
+        }
+      });
+
+      return undefined;
+    });
+
+    console.log(`############ done cleaning ${table}`);
+    console.log();
+    console.log();
+  });
+};
+
+export default queryTable;
