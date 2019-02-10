39bdf2b5012bfe4bd16e2ee62ba3b27be37e6bb1
Obotha <khyper234@gmail.com>
2019-02-10 04:17:50 +0300

    Fix unfound import

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import 'babel-polyfill'; // required to make promises work
 import mysql from 'mysql';
 
-import queryTable from './utils/queryTable';
+// eslint-disable-next-line import/no-unresolved
+import queryTable from '../utils/queryTable';
 
 const CONNECTION = mysql.createConnection({
   host: 'localhost',
