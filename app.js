8beaf59b1844a11a312f4f7b92603503e3f11dac
Obotha <khyper234@gmail.com>
2019-02-10 04:27:26 +0300

    Abandon exporting

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import 'babel-polyfill'; // required to make promises work
 import mysql from 'mysql';
 
-// eslint-disable-next-line import/no-unresolved
-import queryTable from '../utils/queryTable';
 
 const CONNECTION = mysql.createConnection({
   host: 'localhost',
@@ -19,7 +17,39 @@ CONNECTION.connect((connectionError) => {
   console.log('CONNECTION ESTABLISHED ************************* ========================== *********************** ');
 });
 
-queryTable('GroundNode', CONNECTION);
-queryTable('SinkNode', CONNECTION);
-queryTable('TenMeterNode', CONNECTION);
-queryTable('TwoMeterNode', CONNECTION);
+
+export default async function queryTable(table) {
+  const query = `SELECT RTC_T,id FROM ${table} WHERE CHAR_LENGTH(RTC_T) > 19`;
+  console.log(`starting to clean ${table}`);
+
+  await CONNECTION.query(query, (queryError, result, fields) => {
+    if (queryError) {
+      throw queryError;
+    }
+    result.map(async (rtcField) => {
+      console.log(`cleaning ${table} data ....`);
+      const { id } = rtcField;
+      const RTC_T = rtcField.RTC_T.slice(-19);
+  
+      const updateQuery = `UPDATE ${table} SET RTC_T = '${RTC_T}' WHERE id = ${id}`;
+  
+      await CONNECTION.query(updateQuery, (updateError, result, fields) => { 
+        if (updateError) {
+          throw updateError;
+        }
+      });
+
+      return undefined;
+    });
+
+    console.log(`############ done cleaning ${table}`);
+    console.log();
+    console.log();
+  });
+};
+
+
+queryTable('GroundNode');
+queryTable('SinkNode');
+queryTable('TenMeterNode');
+queryTable('TwoMeterNode');
